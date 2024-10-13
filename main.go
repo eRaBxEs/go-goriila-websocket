13cccb8ad43dfb322e50b213ab4201acdd621dc4
Ehioje Henry Erabor <ehiojee@yahoo.com>
2024-10-13 23:23:11 +0100

    add basic websocket handler feature to read message and write it back to client

--- main.go
+++ main.go
@@ -2,8 +2,53 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+
+	"github.com/gorilla/websocket"
 )
 
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func handleWebSocket(w http.ResponseWriter, r *http.Request) {
+
+	// Upgrade the HTTP connection to a WebSocket connection.
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer conn.Close()
+
+	// Handle the websocket message here
+	for {
+		messageType, data, err := conn.ReadMessage()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		fmt.Printf("Received Message: %s\n", data)
+
+		// Echo the message back to the client
+		if err := conn.WriteMessage(messageType, data); err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+
+}
+
 func main() {
-	fmt.Println("Hello gorilla websocket!")
+	http.HandleFunc("/ws", handleWebSocket)
+	fmt.Println("Websocket server is running on :8080/ws")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println(err)
+	}
 }
